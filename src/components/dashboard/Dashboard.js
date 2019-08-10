1e8e0ffd0a37efd10df1fa6f162f30126afee452
kalyan.hodersolutions <kalyan.hodersolutions@users.github.com>
2019-08-10 18:41:40 +0530

    new bar
    new bar

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import VanViaBar from '../sidebar/SideBar';
+import VanViaBar from '../layout/sidebar/SideBar';
 
 class Dashboard extends Component {	
 	render() {
@@ -10,18 +10,13 @@ class Dashboard extends Component {
 		}
 		else {	
 			return (
-				<Fragment>
-					<VanViaBar />
+				<Fragment>					
+					<VanViaBar />					
 				</Fragment>
 			)
 		}
 	}
 }
-
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
 
 const mapStateToProps = (state) => {
 	return {
