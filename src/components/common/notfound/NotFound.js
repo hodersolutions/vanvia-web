a8ad379c264ea59eec8e5e828e6e59fef36d520a
kalyan.hodersolutions <kalyan.hodersolutions@users.github.com>
2019-08-11 16:50:33 +0530

    not found done
    not found done

--- src/components/common/notfound/NotFound.js
+++ src/components/common/notfound/NotFound.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import './NotFound.css';
 
 class NotFound extends Component {
     render() {
         return (
-            <div>
-                <div className="site-section not-found">
+            <Fragment>
+                <link href="https://fonts.googleapis.com/css?family=Cabin:400,700" rel="stylesheet" />
+	            <link href="https://fonts.googleapis.com/css?family=Montserrat:900" rel="stylesheet" />
+                <div id="notfound">
                     <div className="notfound">
-                        <div className="notfound-404">                            
+                        <div className="notfound-404">
+                            <h3>Oops! Page not found</h3>
                             <h1><span>4</span><span>0</span><span>4</span></h1>
                         </div>
                         <h2>we are sorry, but the page you requested was not found</h2>
+                        <a href="/"><span className="arrow"></span> Return To Homepage</a>
                     </div>
                 </div>
-            </div>            
+            </Fragment>            
         );
     }
 }
