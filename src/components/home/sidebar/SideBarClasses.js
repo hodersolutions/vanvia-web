c3da209b0c819729181735d8edcb685f16eabb9c
kalyan.hodersolutions <kalyan.hodersolutions@users.github.com>
2019-08-15 22:01:38 +0530

    Update SideBarClasses.js

--- src/components/home/sidebar/SideBarClasses.js
+++ src/components/home/sidebar/SideBarClasses.js
@@ -1,10 +1,10 @@
 const CSS_CLASS = {
     "staff" : "fas fa-user-tie",
     "students": "fas fa-user-graduate",
-    "Finance": "fas fa-money-bill-wave",
-    "Facilities": "fas fa-tools",
-    "Media": "fas fa-photo-video",
-    "Work": "fas fa-clipboard"
+    "finance": "fas fa-money-bill-wave",
+    "facilities": "fas fa-tools",
+    "media": "fas fa-photo-video",
+    "work": "fas fa-clipboard"
 }
 
 export default CSS_CLASS;
