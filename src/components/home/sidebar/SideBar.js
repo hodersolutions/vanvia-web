7d87c2cbc5d76ce91748471eb68afe2ed93472d9
kalyan.hodersolutions <kalyan.hodersolutions@users.github.com>
2019-08-18 12:58:44 +0530

    gradient sidebar
    gradient sidebar

--- src/components/home/sidebar/SideBar.js
+++ src/components/home/sidebar/SideBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Random from '../../common/Helper';
+// import Random from '../../common/Helper';
 import SideBarBrand from './SideBarBrand';
 import SideBarHeader from './SideBarHeader';
 import SideBarSearch from './SideBarSearch';
@@ -9,11 +9,19 @@ import SideBarFooter from './SideBarFooter';
 class SideBar extends Component {
     constructor() {
         super();
-        var imgUrl = require('../../../static/images/sidebar/sidebar_'+ Random() +'.jpg');
+        /* var imgUrl = require('../../../static/images/sidebar/sidebar_'+ Random() +'.jpg'); */
 
 	    this.state = {
             background: {
-                'background': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(' + imgUrl + ')'
+                'boxShadow': 'rgb(0, 0, 0) 4px 0px 6px 0px',
+                'borderRadius': '0px 5px 5px 0px',
+                /* 'background': 'linear-gradient(to right, #3A6073, #16222A)'
+                'background': 'linear-gradient(to right, #414345, #232526)'
+                'background': 'linear-gradient(to right, #29323c, #485563)'
+                'background': 'linear-gradient(to right, #243B55, #141E30)'
+                'background': 'linear-gradient(to right, #004e92, #000428)' */
+                'background': 'linear-gradient(to right, #2C5364, #203A43, #0F2027)'
+                /* 'background': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(' + imgUrl + ')' */                
             } 
         }
     }
